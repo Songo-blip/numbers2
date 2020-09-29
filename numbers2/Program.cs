b2dc89e443b045d7f9e87ab70442e4375137e4c1
Sisipo Songo <ssongo@my365-uwc.ac.za>
2020-09-29 21:08:05 +0200

    Add project files.

---
+++ numbers2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+
+        private static int BiggestNumber(int[] numbers)
+
+        {
+            int i;
+            int max = numbers[0];
+            for (i = 1; i < numbers.Length; i++)
+            if (numbers[i] > max)
+                max = numbers[i];
+
+            return (max); 
+        }
+                   
+         static void Main(string[] args)
+         { 
+            int[] mynumbers = { 2, 1, 7, 4, 9, 5 };
+
+            int biggest = BiggestNumber(mynumbers);
+
+          
+                Console.WriteLine("biggest number is" + biggest);
+         }
+    }
+
+}
+             
+
+
